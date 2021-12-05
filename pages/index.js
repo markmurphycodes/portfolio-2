9889fffa30aa7d8e095a482bd4797517c7d99a67
mark <mark@markmurphy.codes>
2021-12-04 17:12:44 -0800

    static regeneration

--- pages/index.js
+++ pages/index.js
@@ -22,5 +22,6 @@ export async function getStaticProps({ preview = false }) {
   const allPosts = (await getAllPostsForHome(preview)) ?? [];
   return {
     props: { preview, allPosts },
+    revalidate: 60,
   };
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -55,6 +55,7 @@ export async function getStaticProps({ params, preview = false }) {
       post: data?.post ?? null,
       morePosts: data?.morePosts ?? null,
     },
+    revalidate: 30,
   };
 }
 
